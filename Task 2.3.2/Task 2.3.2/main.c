23c970b7ad9bb844bdead6125fa7f3d9b4b91ca8
Hastle <alexandrs.me.com@gmail.com>
2018-10-28 21:10:24 +0300

    create second array and input data sum second array

--- Task 2.3.2/Task 2.3.2/main.c
+++ Task 2.3.2/Task 2.3.2/main.c
@@ -3,7 +3,7 @@
 #define X 100
 #define Y 100
 int main(){
-	int x,y,i,j,q,A[X][Y],B[X][Y],sum = 0;
+	int x,y,i,j,q,z,A[X][Y],B[X][Y],sum = 0;
 	setlocale(LC_ALL,"RUS");
 	printf("Введите кол-во строк: ");
 	if(!scanf("%d", &x)) {
@@ -25,19 +25,40 @@ int main(){
 	printf("\n");
 	for (i=0; i<x; i++) {
 		for (j=0; j<y; j++) {
-			printf("%d   ", A[i][j]);
+			printf("%d	", A[i][j]);
 		}
 		printf("\n");
 	}
 	printf("\n");
 	for (i=0; i<x; i++) {
 		for (j=0; j<y; j++) {
-			for (q=j; q!=y; q++) {
-				sum=A[i][q]+sum;
-			}
+			for (q=j;q<y;q++)
+				sum+=A[i][q];
 			printf("sum=%d\n", sum);
+			B[i][j]=sum;
 			sum=0;
 		}
+	}
+	/*=== for (i=z=0; i<x; i++) {
+		for (j=q=0; j<y; j++) {
+			while ((z!=x)&&(q!=y)) {
+				z++;
+				q++;
+				B[i][j]+=B[z][q];
+			}
+			while ((z!=0)&&(q!=y)) {
+				z--;
+				q++;
+				B[i][j]+=B[z][q];
+			}
+		}
+	} ===*/
+	printf("\n");
+	for (i=0; i<x; i++) {
+		for (j=0; j<y; j++) {
+			printf("%d	", B[i][j]);
+		}
+		printf("\n");
 	}
 	return 0;
 }
