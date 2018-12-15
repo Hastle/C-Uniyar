606d7a216fd69dec07c202d857c050277245546c
Hastle <alexandrs.me.com@gmail.com>
2018-12-15 19:36:54 +0300

    task 1g done

--- Task 1g/Task 1g/main.c
+++ Task 1g/Task 1g/main.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
 
-double recurse(double a1, double q, int n);
+double recurse(int a1, int q, int n);
 
 int main() {
-    double a1=0, q=0;
-    int n=0;
+    int a1=0, q=0, n=0;
     printf("Введите начальное значение прогрессии: ");
-    scanf("%lf", &a1);
+    scanf("%d", &a1);
     printf("Введите разность прогрессии: ");
-    scanf("%lf", &q);
+    scanf("%d", &q);
     printf("Введите кол-во элементов прогрессии: ");
     scanf("%d", &n);
-     
+    
     printf("Сумма членов прогрессии = %.0lf\n", recurse(a1,q,n));
     printf("\n\n");
     return 0;
 }
 
-double recurse(double a1, double q, int n) {
-    if(n==1) return a1;
-    a1=recurse(a1,q,n-1);
-    return a1+q;
+double recurse(int a1, int q, int n) {
+    if(n==1)
+        return a1;
+    else
+        return a1+=recurse(a1+q, q, n-1);
 }
+// 2 7 12
