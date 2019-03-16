f45a02ed4f3b3999c60a2a966c267702172e76ea
Hastle <alexandrs.me.com@gmail.com>
2019-03-16 11:01:28 +0300

    create task 8.1b shab, sorted array done

---
+++ Task 8.1b shab/Task 8.1b shab/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Task 8.1b shab
+//
+//  Created by Hastle on 16/03/2019.
+//  Copyright © 2019 Hastle. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
