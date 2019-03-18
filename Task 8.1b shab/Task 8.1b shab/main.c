c4977af13ad3f67ab2abc892daba3c0ce55b4d49
Hastle <alexandrs.me.com@gmail.com>
2019-03-18 21:43:05 +0300

    add count for compire

--- Task 8.1b shab/Task 8.1b shab/main.c
+++ Task 8.1b shab/Task 8.1b shab/main.c
@@ -1,15 +1,35 @@
-//
-//  main.c
-//  Task 8.1b shab
-//
-//  Created by Hastle on 16/03/2019.
-//  Copyright © 2019 Hastle. All rights reserved.
-//
-
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+void InsertionSort(int n, int mass[]) {
+    int newElement, location, repos=0;
+    for (int i=1; i<n; i++) {
+        newElement = mass[i];
+        location = i - 1;
+        while(location >= 0 && mass[location] > newElement) {
+            mass[location+1] = mass[location];
+            repos++;
+            location--;
+        }
+        mass[location+1] = newElement;
+    }
+    printf("Кол-во перемещений: %d\n", repos);
+}
+
+int main() {
+    int N;
+    printf("Введите N: ");
+    scanf("%d", &N);
+    int* mass;
+    mass = (int *)malloc(N * sizeof(int));
+    printf("Введите эллементы массива: ");
+    for (int i=0; i<N; i++)
+        scanf("%d", &mass[i]);
+    InsertionSort(N, mass);
+    printf("Сортированный массив: ");
+    for (int i = 0; i < N; i++)
+        printf("%d ", mass[i]);
+    printf("\n\n");
+    free(mass);
     return 0;
 }
